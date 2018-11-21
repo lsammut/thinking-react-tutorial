0ce573ba610bf226ccedb8d35df113b927baa76b
Letizia Sammut <letizia.sammut@synchrotron.org.au>
2018-11-22 10:14:12 +1100

    Use state in props to filter rows in table

--- src/components/product_table.js
+++ src/components/product_table.js
@@ -4,11 +4,24 @@ import ProductRow from '../components/product_row';
 import ProductCategoryRow from '../components/product_category_row';
 
 class ProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
     render() {
+        const filterText = this.props.filterText;
+        const inStockOnly = this.props.inStockOnly;
+
         const rows = [];
         let lastCategory = null;
         
         this.props.products.forEach((product) => {
+        if (product.name.indexOf(filterText) === -1) {
+            return;
+        }
+        if (inStockOnly && !product.stocked) {
+            return;
+        }
         if (product.category !== lastCategory) {
             rows.push(
             <ProductCategoryRow
