add84872573d3bd4b217f233e05d42a45bf553c2
Letizia Sammut <letizia.sammut@synchrotron.org.au>
2018-11-22 10:08:24 +1100

    Identified and added state to owner component
    Added state to parent component constructor
    Passed state to child components

--- src/components/filterable_table.js
+++ src/components/filterable_table.js
@@ -4,11 +4,22 @@ import SearchBar from '../components/search_bar';
 import ProductTable from '../components/product_table';
 
 export default class FilterableProductTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { // initial state of application
+            filterText: '', 
+            inStockOnly: false}; 
+    }
+
     render() {
         return (
         <div>
-            <SearchBar />
-            <ProductTable products={this.props.products} />
+            <SearchBar 
+                filterText={this.state.filterText}
+                inStockOnly={this.state.inStockOnly}/>
+            <ProductTable products={this.props.products} 
+                filterText={this.state.filterText}
+                inStockOnly={this.state.inStockOnly}/>
         </div>
         );
     }
